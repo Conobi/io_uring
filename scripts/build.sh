ec63082ef52a9ebb6378ced6c38ddbfe7b59cb69
Conobi <5563277+Conobi@users.noreply.github.com>
2026-03-10 11:32:14 +0100

    chore: pin Mojo 0.26.1, add CLAUDE.md, switch build to uvx

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-magic run mojo package io_uring -o io_uring.mojopkg
-magic run mojo package mojix -o mojix.mojopkg
-magic run mojo package linux_raw -o linux_raw.mojopkg
+uvx --from mojo-compiler mojo package linux_raw -o linux_raw.mojopkg
+uvx --from mojo-compiler mojo package mojix -o mojix.mojopkg
+uvx --from mojo-compiler mojo package io_uring -o io_uring.mojopkg
